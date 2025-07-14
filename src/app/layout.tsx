95b72bfe109548f25afaaf682ab42391ad9e5370
alperyz <alperstarr@yahoo.com>
2025-07-14 17:51:58 +0300

    GTM-MSZZLH8P

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -48,7 +48,7 @@ export default function RootLayout({
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-TPSJ63FM');
+            })(window,document,'script','dataLayer','GTM-MSZZLH8P');
           `,
         }} />
         {/* End Google Tag Manager */}
@@ -56,7 +56,7 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {/* Google Tag Manager (noscript) */}
         <noscript>
-          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TPSJ63FM"
+          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MSZZLH8P"
                   height="0" width="0" style={{display:'none',visibility:'hidden'}}></iframe>
         </noscript>
         {/* End Google Tag Manager (noscript) */}
